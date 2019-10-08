ffc58be4b4b774a2a2ad36f1de67d3f0d433798f
rmlzy <liuzhiyuan2018@gmail.com>
2019-10-08 16:19:27 +0800

    feat: Remove Nodejs Version

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-node /Users/go_songs/Documents/liuzhiyuan/quiver2hexo/quiver2hexo.js /Users/go_songs/Documents/liuzhiyuan/QuiverData/github.qvnotebook /Users/go_songs/Documents/liuzhiyuan/blog/yuan-blog/source
-
-cd /Users/go_songs/Documents/liuzhiyuan/blog/yuan-blog
-git add .
-git commit -m '自动同步'
-git push 
-hexo clean
-hexo deploy
